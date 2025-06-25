3edd2614ef2b548f1027600e81b892e90a31bf4e
shimrit alufer <shimrit.alufer@gmail.com>
2025-06-26 01:00:29 +0300

    confirm before delete

--- cmps/TodoList.jsx
+++ cmps/TodoList.jsx
@@ -1,7 +1,16 @@
 import { TodoPreview } from "./TodoPreview.jsx"
 const { Link } = ReactRouterDOM
 
+const { useState } = React
+
 export function TodoList({ todos, onRemoveTodo, onToggleTodo }) {
+
+    const [confirmDeleteId, setConfirmDeleteId] = useState(null)
+
+    function handleRemove(todoId) {
+        onRemoveTodo(todoId)
+        setConfirmDeleteId(null)
+    }
 
     return (
         <ul className="todo-list">
@@ -9,7 +18,15 @@ export function TodoList({ todos, onRemoveTodo, onToggleTodo }) {
                 <li key={todo._id}>
                     <TodoPreview todo={todo} onToggleTodo={()=>onToggleTodo(todo)} />
                     <section>
-                        <button onClick={() => onRemoveTodo(todo._id)}>Remove</button>
+                        {confirmDeleteId === todo._id ? (
+                           <React.Fragment>
+                                <span>Are you sure?</span>
+                                <button onClick={() => handleRemove(todo._id)}>Yes</button>
+                                <button onClick={() => setConfirmDeleteId(null)}>Cancel</button>
+                            </React.Fragment>
+                        ) : (
+                            <button onClick={() => setConfirmDeleteId(todo._id)}>Remove</button>
+                        )}
                         <button><Link to={`/todo/${todo._id}`}>Details</Link></button>
                         <button><Link to={`/todo/edit/${todo._id}`}>Edit</Link></button>
                     </section>
